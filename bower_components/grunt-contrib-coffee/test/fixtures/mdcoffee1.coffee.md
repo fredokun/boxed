0b9d57f94522d5e018b06a3705fd8e8985a6e12b
gitSeed <patrick.andri@gmail.com>
2015-02-04 00:41:55 +0100

    Remove the bower_compoment folder.

--- bower_components/grunt-contrib-coffee/test/fixtures/mdcoffee1.coffee.md
+++
@@ -1,7 +0,0 @@
-Begin function declaration.
-
-    sayHello = ->
-
-Print a greeting.
-
-      console.log 'I like to annotate my source with literated coffee.'
--- bower_components/grunt-contrib-jade/test/fixtures/inc/filters.js
+++
@@ -1,9 +0,0 @@
-var jadefilters = module.exports = {};
-
-jadefilters.some = function(block) {
-  return 'some: ' + block;
-};
-
-jadefilters.another = function(block) {
-  return 'another: ' + block;
-};
